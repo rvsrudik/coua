49197a39509686035794a613ab44d34595484649
Vitalii Rudenko <vitalii@Vitaliis-MacBook-Pro.local>
2025-02-06 20:38:08 -0700

    add clarity

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,4 +10,24 @@ export default defineNuxtConfig({
       tailwindcss(),
     ],
   },
+  // nuxt.config.ts
+
+  app: {
+    head: {
+      script: [
+        {
+          children: `<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "q5wrnvrnnl");
+</script>`,
+          type: 'text/javascript',
+        },
+      ],
+    },
+  },
+
+
 })
