60922d9676891b3116b8037e8b93034c64cacfd8
Vitalii Rudenko <vitalii@Vitaliis-MacBook-Pro.local>
2025-02-11 11:09:51 -0700

    add logo and meta

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="p-4 md:p-6">
         <div class="flex lg:items-center mb-5 flex-col lg:flex-row items-start">
-            <h1 class="uppercase font-bold lg:text-2xl ">Українці в Колорадо | Послуги</h1>
+            <div class="flex items-center">
+                <img src="/public/logo.png" alt="logo" class="w-11 h-11 lg:w-15 lg:h-15 mr-2 lg:mr-4">
+                <h1 class="uppercase font-bold lg:text-2xl ">Українці в Колорадо <hr class=" lg:block"> Послуги</h1>
+            </div>
 
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLScC6JWHsmvn73hG2FwQkdMOpLi840b64vmsm0oC6Xm37PiAGw/viewform?usp=header" target="_blank"> <button class="lg:ml-6 mt-3 lg:mt-0 bg-blue-600 hover:bg-blue-500 px-4 py-2 lg:py-3 text-white uppercase font-bold rounded-3xl cursor-pointer ">Додати послугу</button></a>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLScC6JWHsmvn73hG2FwQkdMOpLi840b64vmsm0oC6Xm37PiAGw/viewform?usp=header" class="w-full lg:w-fit"  target="_blank"> <button class="lg:ml-10 mt-4 lg:mt-0  bg-[#0057b8] w-full lg:w-fit hover:bg-blue-500 px-4 py-2 lg:py-3 text-white uppercase font-bold rounded-3xl cursor-pointer ">Додати послугу</button></a>
         </div>
 
 
@@ -47,6 +50,32 @@ function onClickTag(tag) {
         selectedTags.value.push(tag)
     }
 }
+
+function shuffle(arr) {
+  let currentIndex = array.length;
+  const array = [...arr]
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array
+}
+
+useHead({
+  title: 'Українці в Колорадо | Послуги',
+  meta: [
+    { name: 'description', content: 'Платформа для українців у Колорадо, яка дозволяє знаходити та додавати послуги й сервіси в різних сферах, таких як ремонт, освіта, медицина, доставка, юридична допомога, ІТ-послуги та багато іншого.' }
+  ]
+})
 </script>
 
 <style scoped>
