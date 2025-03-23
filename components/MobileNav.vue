6cb5f577065e875cd18f2b4ca15e649e0d868bad
Vitalii Rudenko <vitalii@Vitaliis-MacBook-Pro.local>
2025-03-23 11:01:13 -0600

    fix ui

--- components/MobileNav.vue
+++ components/MobileNav.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="h-14 bg-white fixed bottom-0 left-0 border-t-1 border-gray-100 shadow-2xl w-full flex justify-center items-center">
-        <ul class="text-lg h-full flex-nowrap md:mx-10 text-gray-700 grid" :class="`grid-cols-${mainNav.length}`">
+        <ul class="text-lg h-full flex-nowrap md:mx-10 text-gray-700 flex w-full" :class="`grid-cols-${mainNav.length}`">
             <li v-for="(menuItem, i) in mainNav" :key="i"  :class="[route.name === menuItem.routeName ? 'bg-gray-200 font-bold' : '']" class="h-full flex items-center justify-center px-3 w-full"><RouterLink :to="{name: menuItem.routeName}">{{ menuItem.title }}</RouterLink></li>
         </ul>
     </div>
